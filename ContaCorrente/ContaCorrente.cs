06b3817ebfc8ba16d391e578f7b22ca832c6e559
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-05-21 19:54:17 -0300

    Organização do código

--- ContaCorrente/ContaCorrente.cs
+++ ContaCorrente/ContaCorrente.cs
@@ -10,13 +10,13 @@ namespace ContaCorrente
 {
     internal class ContaCorrente
     {
-        public string numConta;
-        public float saldo;
-        public bool status;
-        public float limite;
-        public List<List<string>> historico = new List<List<string>>();
+        private string numConta;
+        private float saldo;
+        private bool status;
+        private float limite;
+        private List<List<string>> historico = new List<List<string>>();
 
-        public Cliente titular;
+        private Cliente titular;
 
 
 
@@ -25,81 +25,94 @@ namespace ContaCorrente
 
         public ContaCorrente(string numConta, float saldo, bool status, float limite, Cliente titular)
         {
+            this.numConta = numConta;
             this.saldo = saldo;
             this.status = status;
             this.limite = limite;
             this.titular = titular;
             inicializaHistorico();
         }
-
+        //inicializa o historico na criação da conta, para criar duas listas dentro da lista
         private void inicializaHistorico()
         {
             historico.Add(new List<string>());
             historico.Add(new List<string>());
         }
-
-        public void sacar(float valor)
-        {
-            if (verificarSaldo(valor))
-            {
-                this.saldo -= valor;
-                insereHistorico(valor, debito);
-                Console.WriteLine("Saldo atual: R$" + getSaldo());
-            } else
-            {
-                Console.WriteLine("Não é possível sacar mais que o limite disponível! ");
-            }
-        }
-
-        public void depositar(float valor)
-        {
-            this.saldo += valor;
-            insereHistorico(valor, credito);
-            Console.WriteLine("Saldo atual: R$" + getSaldo());
-        }
-
+        //getSaldo
         public float getSaldo()
         {
             return this.saldo;
         }
 
+
+
+        //função para sacar
+        public void sacar(float valor)
+        {
+            //verifica se é possivel realizar o saque com o valor recebido
+            if (verificarSaldo(valor))
+            {
+                //retira o valor da conta e adiciona no historico
+                this.saldo -= valor;
+                insereHistorico(valor, debito);
+                Console.WriteLine("Saldo atual: R$" + getSaldo());
+            } else
+            {
+                //informa que não tem limite
+                Console.WriteLine("Não é possível sacar mais que o limite disponível! ");
+            }
+        }
+        //função para depositar
+        public void depositar(float valor)
+        {
+            //adiciona o valor na conta e no histórico
+            this.saldo += valor;
+            insereHistorico(valor, credito);
+            Console.WriteLine("Saldo atual: R$" + getSaldo());
+        }
+
+        //verifica se é possivel realizar saque ou transferencia
         private bool verificarSaldo(float valor)
         {
+            //retorna TRUE/FALSE caso o valor que queira sacar for menor 
             return valor <= getSaldo() + limite ? true : false;
         }
 
-        public ArrayList getExtrato()
-        {
-            return null;
-        }
-        public void receberTransf(float valor)
-        {
-            this.saldo += valor;
-            insereHistorico(valor, credito);
-        }
-
+        //transfere dinheiro, recebe a conta de destino e o valor de parametro
         public void transferirDinheiro(ContaCorrente contaDestino, float valor)
         {
             if (verificarSaldo(valor))
             {
+                //retira o saldo da conta que envia e adiciona no historico
                 this.saldo -= valor;
                 insereHistorico(valor, debito);
+                //chama a função para receber a transferencia na conta destino, enviando o valor
                 contaDestino.receberTransf(valor);
                 Console.WriteLine("Valor transferido com sucesso! ");
-            } else
+            }
+            else
             {
-                Console.WriteLine("Não é possível transferir mais que o limite! "); 
+                Console.WriteLine("Não é possível transferir mais que o limite! ");
             }
         }
 
+        //função para receber a transferencia de outra conta
+        public void receberTransf(float valor)
+        {
+            //adiciona o valor no saldo e adiciona no historico
+            this.saldo += valor;
+            insereHistorico(valor, credito);
+        }
 
-
+        //adiciona o valor e o tipo da transferencia no histórico
+        //tipo = credito/debito
         private void insereHistorico(float valor, string tipo)
         {
             historico[0].Add(Convert.ToString(valor));
             historico[1].Add(tipo);
         }
 
+        //imprime o extrato 
         public void imprimirExtrato()
         {
             Console.WriteLine($"Extrato do {titular.nome} conta numero: {numConta}");
